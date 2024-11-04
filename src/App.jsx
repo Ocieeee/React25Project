76d8b7e02a37a45400ec2e0f2c10452074bd81a9
Om chaudhari <omchaudhari0003@gmail.com>
2024-11-04 21:06:25 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+// import ImageSlider from "./components/image-slider";
+// import LoadMoreData from "./components/load-more-data";
+// import StarRating from "./components/star-rating";
+// // import Accordian from "./components/accordian";
+// // import RandomColor from "./components/randomcolor";
+
+function App() {
+  return (
+    <div>
+      {/* <Accordian/> */}
+      {/* <RandomColor /> */}
+      {/* <StarRating  noOfStars={10}/> */}
+      {/* <ImageSlider url={"https://picsum.photos/v2/list"} limit={"7"} page={5} /> */}
+      {/* <LoadMoreData /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/accordian/data.js
@@ -0,0 +1,28 @@
+const data = [
+  {
+    id: "1",
+    question: "What is accordian ?",
+    answer:
+      "Yes of course, it is very possbile to create an accordian components with another framwork",
+  },
+  {
+    id: "2",
+    question: "What are they use for ?",
+    answer:
+      "Yes of course, it is very possbile to create an accordian components with another framwork",
+  },
+  {
+    id: "3",
+    question: "Accordian as a musical instrument ?",
+    answer:
+      "Yes of course, it is very possbile to create an accordian components with another framwork",
+  },
+  {
+    id: "4",
+    question: "Can I Create an accordian with different framwork ?",
+    answer:
+      "Yes of course, it is very possbile to create an accordian components with another framwork",
+  },
+];
+
+export default data;
